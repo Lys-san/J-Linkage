0cb0547bd7a008903f64e34a4846ac16943ca725
Lys-san <lysandre.macke@gmail.com>
2023-05-02 09:28:50 +0200

    just testing git configurations
    added gitignore file for some cleanup

--- CMakeFiles/jlk.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for jlk.
-# This may be replaced when dependencies are built.
--- CMakeFiles/jlk.dir/window.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/jlk.dir/window.cpp.o: \
- /home/lysandre/Documents/tests/JLinkage/window.cpp \
- /usr/include/stdc-predef.h \
- /home/lysandre/Documents/tests/JLinkage/window.h
